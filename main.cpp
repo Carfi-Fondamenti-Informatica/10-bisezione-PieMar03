cc9db30504505a3cba870397c19982f9e14f4bd8
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-12-20 10:50:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
+float f(float y){
+    float x = pow(y, 2) * cos(y) + 1;
+    return x;
+}
 int main() {
-   return 0;
+    float a = 0, b = 0, x = 0, err = 0;
+    do{
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+        }while(f(a) * f(b) >= 0);
+    do{
+        x=(a + b) / 2;
+        if(f(x) == 0){
+            cout << x << endl;
+            cout << f(x);
+            return 0;
+        }
+        else{
+            if(f(a) * f(x) < 0){
+                b = x;
+            }
+            else{
+                a = x;
+            }
+            err = abs((b - a) / 2);
+        }
+  }while(err >= 1e-6);
+    cout << int(x * 10000) / 10000.0 <<endl;
+    return 0;
 }
